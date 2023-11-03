f7dbac67aec6e477127699cb55166da669fe4042
cloudguardian2023 <cloudguardian2023@gmail.com>
2023-11-04 00:03:26 +0900

    feat: 링크 처리 예제 페이지 컨트롤러 코드 작성

--- src/main/java/com/example/shop/controller/ThymeleafExController.java
+++ src/main/java/com/example/shop/controller/ThymeleafExController.java
@@ -68,4 +68,9 @@ public class ThymeleafExController {
         return "thymeleafEx/thymeleafEx04";
     }
 
+    @GetMapping(value = "/ex05")
+    public String thymeleafExample05() {
+        return "thymeleafEx/thymeleafEx05";
+    }
+
 }
